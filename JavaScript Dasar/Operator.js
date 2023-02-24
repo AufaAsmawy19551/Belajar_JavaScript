94c3447d42fc7436fc02e90d1c7481f774bd31f9
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-02-24 20:04:03 +0700

    update

---
+++ JavaScript Dasar/Operator.js
@@ -0,0 +1,71 @@
+// Unary Operator
+
+//Numeric Convertion
+let a = 10;  
+let b = -20;  
+console.log(-a);
+console.log(-b);
+
+//Increment
+let c = 10;
+let d = 10;
+console.log(++c);
+console.log(d++);
+
+//Decrement 
+let e = 10;
+let f = 10;
+console.log(--e);
+console.log(f--);
+
+//Binary Operator
+
+//Asigment
+let g = 10;
+console.log(g);
+console.log(g += 1);
+console.log(g -= 1);
+console.log(g *= 2);
+console.log(g /= 2);
+console.log(g %= 7);
+console.log(g **= 2);
+
+//Aritmatika
+console.log(3 + 2);
+console.log(3 - 2);
+console.log(3 * 2);
+console.log(3 / 2);
+console.log(3 % 2);
+console.log(3 ** 2);
+ 
+//Perbandingan
+console.log(3 < 2);
+console.log(3 > 2);
+console.log(3 <= 2);
+console.log(3 >= 2);
+console.log(3 == 2);
+console.log(3 === 2);
+console.log(3 != 2);
+
+//Logika
+console.log(true || true);
+console.log(false || true);
+console.log(true || false);
+console.log(false || false);
+
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+console.log(false && false);
+
+//Concatination 
+let h = "Aufa";
+let i = "Asmawy";
+console.log(h + " " + i);
+
+//Ternary Operator
+
+//Conditional
+let j = 10;
+let k = 20;
+console.log(j > k ? "h lebih besar dari i" : "h kurang dari i" );
