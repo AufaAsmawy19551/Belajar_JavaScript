31174c648cab65c2ff0990e02be35b40eae9a541
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-03 01:42:11 +0700

    update

---
+++ JavaScript Standard Library/Set.js
@@ -0,0 +1,13 @@
+const set = new Set();
+
+set.add("Eko");
+set.add("Joko");
+set.add("Budi");
+set.add("Eko");
+set.add("Edi");
+set.add("Joko");
+set.add("Budi");
+set.add("Edi");
+
+console.log(set);
+set.forEach(value => console.log(value));
