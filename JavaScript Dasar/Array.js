81f3ad04ce0d47a8f16837f461ddfd4d739ab63e
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-02 21:31:16 +0700

    update

--- JavaScript Dasar/Array.js
+++ JavaScript Dasar/Array.js
@@ -6,13 +6,13 @@ console.log(fruits[1]); //apple
 console.log(fruits[2]); //banana
 console.log(fruits[3]); //orange
 console.log(fruits[4]); //grape
-console.table(fruits); 
-// ┌─────────┬──────────┐ 
-// │ (index) │  Values  │ 
-// ├─────────┼──────────┤ 
-// │    0    │ 'manggo' │ 
-// │    1    │ 'apple'  │ 
-// │    2    │ 'banana' │ 
-// │    3    │ 'orange' │ 
-// │    4    │ 'grape'  │ 
-// └─────────┴──────────┘ 
+console.table(fruits);
+// ┌─────────┬──────────┐
+// │ (index) │  Values  │
+// ├─────────┼──────────┤
+// │    0    │ 'manggo' │
+// │    1    │ 'apple'  │
+// │    2    │ 'banana' │
+// │    3    │ 'orange' │
+// │    4    │ 'grape'  │
+// └─────────┴──────────┘
