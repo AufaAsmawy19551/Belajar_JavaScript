957aec6da74b71b34042482778fc229c5941b789
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-02 21:32:48 +0700

    update

--- JavaScript Dasar/function.js
+++ JavaScript Dasar/function.js
@@ -1,33 +1,33 @@
 function volumeKubus(s) {
-  return s ** 3
+  return s ** 3;
 }
 
 let volumeBalok = function (a, b, c) {
-  return a * b * c
-}
+  return a * b * c;
+};
 
 let volumeBola = (r) => {
-  return (4 / 3) * 3.14 * r ** 3
-}
+  return (4 / 3) * 3.14 * r ** 3;
+};
 
-console.log(volumeKubus(10))
-console.log(volumeBalok(10, 20, 30))
-console.log(volumeBola(10))
+console.log(volumeKubus(10));
+console.log(volumeBalok(10, 20, 30));
+console.log(volumeBola(10));
 
 function countLength(arr, callback) {
-  newArr = []
-  arr.forEach((e) => newArr.push(callback(e)))
-  return newArr
+  newArr = [];
+  arr.forEach((e) => newArr.push(callback(e)));
+  return newArr;
 }
 
-let arr = ['Muhammad', 'Aufa', 'Asmawy']
+let arr = ['Muhammad', 'Aufa', 'Asmawy'];
 
-console.log(countLength(arr, (e) => e.length))
+console.log(countLength(arr, (e) => e.length));
 
 //Rest parameter
-function sum(...data){
+function sum(...data) {
   let total = 0;
-  data.forEach((e) => total += e)
+  data.forEach((e) => (total += e));
   return total;
 }
 
