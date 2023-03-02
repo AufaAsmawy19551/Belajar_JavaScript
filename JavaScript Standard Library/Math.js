741791a1001027819f1415205764583ff905295f
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-03 00:05:40 +0700

    update

---
+++ JavaScript Standard Library/Math.js
@@ -0,0 +1,8 @@
+console.log(Math.max(10, 11, 9, 13, 8));
+console.log(Math.min(10, 11, 9, 13, 8));
+console.log(Math.round(10.5));
+console.log(Math.round(10.3));
+console.log(Math.floor(10.3));
+console.log(Math.floor(10.6));
+console.log(Math.ceil(10.3));
+console.log(Math.ceil(10.6));
