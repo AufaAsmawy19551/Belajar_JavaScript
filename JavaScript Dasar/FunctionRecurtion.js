75141c32ed05b8579f0dc3ed76044e6a167a0c8d
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-02 21:34:45 +0700

    update

--- JavaScript Dasar/FunctionRecurtion.js
+++ JavaScript Dasar/FunctionRecurtion.js
@@ -1,9 +1,9 @@
-const recursive = number => {
-  if(number === 1 || number === 0){
+const recursive = (number) => {
+  if (number === 1 || number === 0) {
     return 1;
-  }else{
+  } else {
     return number * recursive(number - 1);
   }
-}
+};
 
 console.log(recursive(5));
