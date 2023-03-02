83ec20252820b4a70e311e846ffb5557f81fb272
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-03 00:06:20 +0700

    update

---
+++ JavaScript Standard Library/String.js
@@ -0,0 +1,9 @@
+const name = 'Muhammad Aufa Asmawy';
+
+console.log(name.length);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.split(' '));
+
+const input = '   input   ';
+console.log(input.trim());
