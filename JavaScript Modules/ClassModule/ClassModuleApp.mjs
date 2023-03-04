3c12edfd3a88b4497a9049c72a38de52a0a6be29
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-04 13:29:03 +0700

    update

---
+++ JavaScript Modules/ClassModule/ClassModuleApp.mjs
@@ -0,0 +1,10 @@
+import { Employee, Manager } from './ClassModule.mjs';
+const eko = new Employee('Eko', 'Jakarta');
+eko.sayHello('Joko');
+
+const joko = new Manager('Joko', 'Yogyakarta', 'Marketing');
+joko.sayHello('Eko');
+joko.superSayHello('Eko');
+
+console.log(eko);
+console.log(joko);
