e5628ff51340e4a5f7654e78d8b76ff2c1300d45
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-04 13:28:40 +0700

    update

---
+++ JavaScript Modules/ClassModule/ClassModule.mjs
@@ -0,0 +1,25 @@
+export class Employee {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  sayHello(name) {
+    console.log(`Hello ${name}, My name is Employee ${this.name}`);
+  }
+}
+
+export class Manager extends Employee {
+  constructor(name, address, department) {
+    super(name, address);
+    this.department = department;
+  }
+
+  sayHello(name) {
+    console.log(`Hello ${name}, My name is Manager ${this.name}`);
+  }
+
+  superSayHello(name) {
+    super.sayHello(name);
+  }
+}
