9ceb8576f85a20b6bf73bbb5fada349cd4d480e0
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-04 14:36:39 +0700

    update

---
+++ JavaScript Modules/AggregateModule/AggregateModule.mjs
@@ -0,0 +1,3 @@
+export { name, address } from '../VariableModule/VariableModule.mjs';
+export { sayHello, sayGoodBye } from '../FunctionModule/FunctionModule.mjs';
+export { Employee, Manager } from '../ClassModule/ClassModule.mjs';
