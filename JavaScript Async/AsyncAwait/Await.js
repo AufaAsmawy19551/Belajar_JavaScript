b1bb3af6dad769ace0b3197d38e36470f2241513
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-05 01:18:51 +0700

    update

---
+++ JavaScript Async/AsyncAwait/Await.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function getProducts() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./JavaScript Async/AsyncAwait/products.json', { encoding: 'utf-8' }, (err, data) => {
+      if (data) {
+        resolve(JSON.parse(data));
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+function getProductNamesPromise() {
+  return new Promise((resolve, reject) => {
+    getProducts()
+      .then((products) => {
+        resolve(
+          products.map((product) => {
+            return product.title;
+          })
+        );
+      })
+      .catch((err) => reject(err));
+  });
+}
+
+async function getProductNamesAsync() {
+  const products = await getProducts()
+  return products.map((product) => {
+    return product.title;
+  });
+}
+
+getProductNamesPromise().then((productName) => {
+  console.log(productName);
+});
+getProductNamesAsync().then((productName) => {
+  console.log(productName);
+});
