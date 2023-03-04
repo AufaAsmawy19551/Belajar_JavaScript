ba6497189f2440fde6ab73d3592743902e8f4bb6
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-04 14:13:44 +0700

    update

---
+++ JavaScript Modules/ExportDefault/DefaultFunction.mjs
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  console.log(`Hello, ${name}`);
+}
+
+function sayGoodBye(name) {
+  console.log(`Good bye, ${name}`);
+}
+
+export default sayHello;
+export { sayGoodBye };
