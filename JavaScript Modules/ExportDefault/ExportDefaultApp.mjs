8a6d365948ce49d786636ca85c395631c3af2e92
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-04 14:14:05 +0700

    update

---
+++ JavaScript Modules/ExportDefault/ExportDefaultApp.mjs
@@ -0,0 +1,19 @@
+import author, { address } from './DefaultVariable.mjs';
+import sayHello, { sayGoodBye } from './DefaultFunction.mjs';
+import Employee, { Manager } from './DefaultClass.mjs';
+
+console.log(`name: ${author}`);
+console.log(`address: ${address}`);
+
+sayHello('Eko');
+sayGoodBye('Eko');
+
+const eko = new Employee('Eko', 'Jakarta');
+eko.sayHello('Joko');
+
+const joko = new Manager('Joko', 'Yogyakarta', 'Marketing');
+joko.sayHello('Eko');
+joko.superSayHello('Eko');
+
+console.log(eko);
+console.log(joko);
